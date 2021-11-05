8a36768a4fcd18a78779799458ee907107f99846
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-05 16:25:10 +0530

    Players Poitions

---
+++ SnakeAndLadder.java
@@ -0,0 +1,38 @@
+public class SnakeAndLadder{
+
+
+        public static void main(String[] args) {
+
+
+		int position = 0;
+		System.out.println(" Moksha Patam - Welcome to the game ! Your Current Position = " + position);
+                           
+                double dice = Math.floor( (Math.random() * 10) % 6 + 1); 
+                System.out.println("Dice value is = " +dice);
+ 
+                   int option = (int)Math.floor(Math.random() * 3);
+
+
+
+		if (option == 0) {
+			
+                    System.out.println("No movement ,stay on the same place");
+		}else if(option == 1){
+		
+               	    System.out.println(" Ladder Oh Yes !");
+			position += dice;
+		
+ 	            System.out.println("Your Current Position is " +position);
+		}
+                else {
+			
+                     System.out.println("Oh no! Snake");
+			position -= dice;
+		
+         	    System.out.println(" Your Current Position is " +position);
+
+	}
+
+    }
+
+}
