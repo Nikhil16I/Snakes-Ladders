cba0bb8fe642c7b43ddc7e93fd150319c65c2923
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-05 18:48:47 +0530

    Added diceroll time to win game

---
+++ SnakeAndLadder.java
@@ -0,0 +1,54 @@
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		
+		int position = 0;
+
+
+		System.out.println(" Moksh Patam - Welcome to the game ! ");
+		
+		
+		int dice;
+		int option;
+		int times=0;
+		
+
+		while (position < 100) {
+	        		            dice = (int)Math.floor( (Math.random() * 10) % 6 + 1);
+ 
+		        	            System.out.println("Dice value is - " + dice);
+
+			                    option = (int)Math.floor(Math.random() * 3);
+
+
+                                      switch(option) {
+			case 0: 
+				System.out.println(" No Movement Stay the same");
+				break;
+			case 1: 
+				System.out.println(" ladder Oh yes!");
+				position += dice;
+				if (position > 100) {
+					position -= dice;
+					System.out.println(" counting not more than 100");
+				}
+				break;
+			case 2: 
+				System.out.println(" Oh no ! snake");
+				position -= dice;
+				position = position < 0 ? 0 : position;
+				break;
+							
+			}
+							
+			
+			System.out.println("Your Current position - " + position);
+			times++;
+		}
+		
+		System.out.println("Congratulations!- You won the game in "  +times+ " roll dice times");
+                
+	   }
+
+   }
