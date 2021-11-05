a3eaec259624de9c0eaaed11fde0ee67b1a6292c
Nikhil <92767519+Nikhil16I@users.noreply.github.com>
2021-11-05 16:36:30 +0530

    Update SnakeAndLadder.java

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -37,22 +37,4 @@ public class SnakeAndLadder{
     }
 
 
-		System.out.println(" Moksha Patam - Welcome to the game ! Your Current Position = " + position);
-
-                           double dice = Math.floor( (Math.random() * 10) % 6 + 1); 
-
-                                System.out.println("Dice value is = " +dice);
-                                      
-        public static void main(String[] args) {
 		
-		
-		int position = 0;
-
-		System.out.println(" Moksha Patam - Welcome to the game ! Your Current Position - " + position);
-		
-		
-
-   }
-
-
-}
