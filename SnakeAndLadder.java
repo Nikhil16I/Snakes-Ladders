aca9c8f620357e7394a9b450fa34f72f22e2092b
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-05 18:18:47 +0530

    Exact winning at 100

---
+++ SnakeAndLadder.java
@@ -0,0 +1,50 @@
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		
+		int position = 0;
+		System.out.println(" Moksha Patam - Welcome to the game ! Your Current Position =  ");
+		
+		
+		int dice;
+		int option;
+		
+		
+		while (position < 100) {
+	        		            dice = (int)Math.floor( (Math.random() * 10) % 6 + 1);
+ 
+		        	            System.out.println("Dice value is - " + dice);
+
+			                    option = (int)Math.floor(Math.random() * 3);
+
+
+                                      switch(option) {
+			case 0: 
+				System.out.println(" No Movement Stay the same");
+				break;
+			case 1: 
+				System.out.println(" ladder Oh yes!");
+				position += dice;
+				if (position > 100) {
+					position -= dice;
+					System.out.println(" counting not more than 100");
+				}
+				break;
+			case 2: 
+				System.out.println(" Oh no ! snake");
+				position -= dice;
+				position = position < 0 ? 0 : position;
+				break;
+							
+			}
+							
+			
+			System.out.println("Your Current position - " + position);
+			
+		}
+		
+		System.out.println("Congratulations!- You won the game");
+	}
+
+}
